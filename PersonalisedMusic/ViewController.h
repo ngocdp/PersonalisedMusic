6467fee77e08bc18ea5160fd5f8e498c4d4594a8
Vlad Litvinenko <user@MacBook-Air-Vlad.local>
2017-05-07 05:10:52 +1000

    Initial Commit

---
+++ PersonalisedMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonalisedMusic
+//
+//  Created by Vlad Litvinenko on 07.05.17.
+//  Copyright © 2017 Vlad Litvinenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PersonalisedMusicTests/PersonalisedMusicTests.m
@@ -0,0 +1,39 @@
+//
+//  PersonalisedMusicTests.m
+//  PersonalisedMusicTests
+//
+//  Created by Vlad Litvinenko on 07.05.17.
+//  Copyright © 2017 Vlad Litvinenko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PersonalisedMusicTests : XCTestCase
+
+@end
+
+@implementation PersonalisedMusicTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
